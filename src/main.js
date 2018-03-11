8aae2fe9a3cbe709ec32043b7d9cd895e0751c49
yujieH <yujiehung@gmail.com>
2018-03-11 18:29:15 +0800

    curl noise testing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import './style.css'
 
+
+
+import './style.css'
 // import './p101'
 // import './p102'
 // import './p103'
--- src/s103.js
+++ src/s103.js
@@ -2,16 +2,12 @@
 // Mar 08 2018 yujieH
 
 import * as THREE from 'three'
-import './water.js'
-import './mirror.js'
 import { EffectComposer, GlitchPass, BloomPass, RenderPass } from 'postprocessing'
 import * as dat from 'dat-gui'
 
-import triangulate from 'delaunay-triangulate'
 import SimplexNoise from 'simplex-noise'
 import * as Noise from 'noisejs'
 
-// let noise = new Noise(Math.random)
 let simplex = new SimplexNoise(Math.random)
 let nFactor = 512
 
@@ -34,7 +30,7 @@ const height = Math.floor( window.innerHeight )
 console.log(width)
 console.log(height)
 
-let currentLocation = new THREE.Vector2( Math.floor( Math.random()*width ), Math.floor( Math.random()*height))
+let currentLocation = new THREE.Vector2( Math.random()*width,Math.random()*height)
 
 const init = function (){
   const size = width * height
@@ -43,7 +39,11 @@ const init = function (){
     let k = i*3
     let posY = i%width
     let posX = Math.floor(i/width)
-    if (posX == currentLocation.x && posY == currentLocation.y) {
+    if (posX == currentLocation.x && posY == currentLocation.y||
+      posX == currentLocation.x + 1 && posY == currentLocation.y ||
+      posX == currentLocation.x - 1 && posY == currentLocation.y ||
+      posX == currentLocation.x && posY == currentLocation.y+1 ||
+      posX == currentLocation.x && posY == currentLocation.y-1){
       data[k] = 0
       data[k+1] = 0
       data[k+2] = 0
@@ -77,35 +77,73 @@ const init = function (){
 
 const animate = function(){
   requestAnimationFrame(animate)
-
-  const size = width * height
-
-  //let t_data = Array(255)
-  let eps = 1.0
-  let n1, n2, a, b
-  n1 = simplex.noise2D(currentLocation.x, currentLocation.y + eps,t)
-  n2 = simplex.noise2D(currentLocation.x, currentLocation.y - eps,t)
-  a = (n1 - n2)/(2 * eps)
-  a = Math.floor(a*100)
-  n1 = simplex.noise2D(currentLocation.x+ eps, currentLocation.y ,t )
-  n2 = simplex.noise2D(currentLocation.x- eps, currentLocation.y ,t )
-  b = (n1 - n2)/(2 * eps)
-  b = Math.floor(b*100)
-  let curl = new THREE.Vector2(a,-b)
-  currentLocation.add(curl)
-  let index = (currentLocation.x + currentLocation.y*width) * 3
-  data[index] = 0
-  data[index+1] = 0
-  data[index+2] = 0
-
-  //data.set(t_data)
+  const loopTime = 30000
+  for (var i = 0; i < loopTime; i++) {
+    drawCurl()
+  }
   texture.needsUpdate = true
   material.needsUpdate = true
   renderer.render(scene, camera)
-  console.log( curl.x+ ','+ curl.y + ' : ' +currentLocation.x + ',' + currentLocation.y)
-  t+=1
-}
 
+
+}
+const drawCurl = function(){
+  const size = width * height
+  let eps = 1.0
+  let n1, n2, a, b
+  n1 = simplex.noise3D(currentLocation.x, currentLocation.y + eps, t)
+  n2 = simplex.noise3D(currentLocation.x, currentLocation.y - eps, t)
+  // n1 = Math.abs(n1)
+  // n2 = Math.abs(n2)
+  a = (n1 - n2)/(2 * eps) * 5
+  n1 = simplex.noise3D(currentLocation.x+ eps, currentLocation.y, t)
+  n2 = simplex.noise3D(currentLocation.x- eps, currentLocation.y, t)
+  b = (n1 - n2)/(2 * eps) * 5
+  // n1 = Math.abs(n1)
+  // n2 = Math.abs(n2)
+  let curl = new THREE.Vector2(a,-b)
+
+  currentLocation.add(curl)
+  //console.log( curl.x+ ','+ curl.y + ' : ' +currentLocation.x + ',' + currentLocation.y)
+  if (currentLocation.x <  0) {
+    currentLocation.x += width
+  }else if(currentLocation.x > width){
+    currentLocation.x -= width
+  }
+  if (currentLocation.y <  0) {
+    currentLocation.y += height
+  }else if(currentLocation.y > height){
+    currentLocation.y -= height
+  }
+
+  // let gray = (simplex.noise2D(t*0.00000001,t*0.0000001) + 1)* 128
+  let gray = Math.floor(t*0.00001)%192
+  let index = ( Math.round(currentLocation.x) + Math.round(currentLocation.y)*width)
+  data[index*3] = gray
+  data[index*3+1] = gray
+  data[index*3+2] = gray
+  //
+  // let index_x1 = index +1
+  // data[index_x1*3] = gray
+  // data[index_x1*3+1] = gray
+  // data[index_x1*3+2] = gray
+  //
+  // let index_x2 = index -1
+  // data[index_x2*3] = gray
+  // data[index_x2*3+1] = gray
+  // data[index_x2*3+2] = gray
+  //
+  // let index_y1 = index + width
+  // data[index_y1*3] = gray
+  // data[index_y1*3+1] = gray
+  // data[index_y1*3+2] = gray
+  //
+  // let index_y2 = index - width
+  // data[index_y2*3] = gray
+  // data[index_y2*3+1] = gray
+  // data[index_y2*3+2] = gray
+  t += 0.9
+}
 
 const onWindowResize = function(){
 
